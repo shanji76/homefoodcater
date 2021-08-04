306a604756aa54da9cdd22a7e9c6246b5a9d93b6
Seema Hanji <seema.hanji@gmail.com>
2021-08-03 22:47:56 -0400

    Initial check in

---
+++ user-register/src/main/java/com/jh/fcs/userregistration/handler/UserRegistrationHandler.java
@@ -0,0 +1,35 @@
+package com.jh.fcs.userregistration.handler;
+
+import com.jh.fcs.userregistration.api.UserApi;
+import com.jh.fcs.userregistration.model.Status;
+import com.jh.fcs.userregistration.model.User;
+import io.swagger.annotations.ApiParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+public class UserRegistrationHandler implements UserApi {
+
+    @RequestMapping(value = "/user/register",
+            produces = { "application/json" },
+            consumes = { "application/json" },
+            method = RequestMethod.POST)
+    public ResponseEntity<Status> registerUser
+            (@ApiParam(value = "" ,required=true )
+             @Valid @RequestBody User user) {
+
+        return ResponseEntity.ok(buildStatus());
+    }
+
+    private Status buildStatus() {
+        Status status = new Status();
+        status.setStatus("SUCCESS");
+        status.setMessage("Successfully Registered User.");
+        return status;
+    }
+
+}
