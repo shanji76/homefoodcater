53d97fb0a8b9ea222252a2fa62e048ae577b12ec
Seema Hanji <seema.hanji@gmail.com>
2021-08-04 00:21:31 -0400

    Enabled actuator

--- user-register/src/main/java/com/jh/fcs/userregistration/handler/UserRegistrationHandler.java
+++ user-register/src/main/java/com/jh/fcs/userregistration/handler/UserRegistrationHandler.java
@@ -9,9 +9,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@RestController
 public class UserRegistrationHandler implements UserApi {
 
     @RequestMapping(value = "/user/register",
