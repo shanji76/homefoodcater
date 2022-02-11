76fbe3b2c51e47edb86b048e020f299fe62c09fc
Seema Hanji <seema.hanji@gmail.com>
2022-02-10 23:36:05 -0500

    Updated config

--- user-register/src/main/java/com/jh/fcs/userregistration/UserRegistrationApplication.java
+++ user-register/src/main/java/com/jh/fcs/userregistration/UserRegistrationApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserRegistrationApplication {
 
-    public static void main(String[] args) {
+    public static void main(String [] args) {
 
         SpringApplication.run(UserRegistrationApplication.class, args);
     }
